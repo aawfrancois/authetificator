310394a6d97730eb6001f9aa4834ed7cea9606b7
aawfrancois <aawfrancois@gmail.com>
2018-04-19 12:15:44 +0200

    add button

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, head} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+
+    constructor(props) {
+        super(props)
+        this.state = {count: 0}
+    }
+
+    onPress = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity
+                    style={styles.buttonAdd}
+                >
+                    <Text> Add </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.buttonClear}
+                >
+                    <Text> Clear </Text>
+                </TouchableOpacity>
+                {/*<View style={[styles.countContainer]}>
+                 <Text style={[styles.countText]}>
+                 { this.state.count !== 0 ? this.state.count : null}
+                 </Text>
+                 </View>*/}
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        paddingHorizontal: 10
+    },
+    buttonAdd: {
+        alignItems: 'center',
+        backgroundColor: '#8bc900',
+        padding: 10,
+        marginBottom: 30,
+        marginTop: 50
+    },
+    buttonClear: {
+        alignItems: 'center',
+        backgroundColor: '#e81f3f',
+        padding: 10
+    },
+    countContainer: {
+        alignItems: 'center',
+        padding: 10
+    },
+    countText: {
+        color: '#FF00FF'
+    }
 });
